91828ceee01d4232521ddd1eba6582113ed04682
Daniel Toro <d.toro@outlook.com>
2018-02-13 19:46:12 -0300

    third commit

--- src/helpers/validations.js
+++ src/helpers/validations.js
@@ -19,19 +19,18 @@ export default {
     if (error) {
       switch (error.details[0].context.key) {
         case 'email':
-          res.status(400)
-            .send({ error: 'You must provide a valid email address' });
+          res.status(400).send({ error: 'You must provide a valid email address' });
           break;
         case 'password':
           res.status(400)
             .send({ error: `
-              The password provided failed to match the following rules: 
-              <br>
-              1. It must contain ONLY the following characters: lower case, upper case, numerics.
-              <br>
-              2. It must be at least 8 characters in length and not greater than 32 characters in length.
-              <br>
-              `
+            The password provided failed to match the following rules: 
+            <br>
+            1. It must contain ONLY the following characters: lower case, upper case, numerics.
+            <br>
+            2. It must be at least 8 characters in length and not greater than 32 characters in length.
+            <br>
+            `
             });
           break;
         default:
